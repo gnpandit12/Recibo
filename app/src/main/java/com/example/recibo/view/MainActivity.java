79404b8d5c4487806d61ffc53c54c460a3b1ef5a
gnpandit12 <gnpandit12@gmail.com>
2021-04-29 21:15:54 +0530

    Update

--- app/src/main/java/com/example/recibo/view/MainActivity.java
+++ app/src/main/java/com/example/recibo/view/MainActivity.java
@@ -25,6 +25,7 @@ import com.example.recibo.viewModel.UserDetailsViewModel;
     private TextView firstNameTextView, lastNameTextView, mobileNoTextView;
     private Button saveDetailsButton;
     private UserDetailsViewModel mUserDetailsViewModel;
+    private String userFirstName, userLastName, userMobileNumber;
     private UserDetails userDetails;
     private String firstNameString = "First Name";
     private String lastNameString = "Last Name";
@@ -49,26 +50,38 @@ import com.example.recibo.viewModel.UserDetailsViewModel;
 
             Log.d(TAG, "Get user details...");
 
-            //Set text view's with user details if not null...
-            if(!TextUtils.isEmpty(userDetails.getFirstName())){
-                Log.d(TAG, "First name: "+userDetails.getFirstName());
-                firstNameTextView.setText(userDetails.getFirstName());
+            if (userDetails != null){
+                userFirstName = userDetails.getFirstName();
+                userLastName = userDetails.getLastName();
+                userMobileNumber = userDetails.getMobileNumber();
+
+                Log.d(TAG, "First name: "+userFirstName);
+                Log.d(TAG, "Last name: "+userLastName);
+                Log.d(TAG, "Mobile number: "+userMobileNumber);
+
+                //Set text view's with user details...
+                if(!TextUtils.isEmpty(userFirstName)){
+                    firstNameTextView.setText(userFirstName);
+                }else {
+                    Log.d(TAG, "First name null");
+                    firstNameTextView.setText(firstNameString);
+                }
+                if(!TextUtils.isEmpty(userLastName)){
+                    lastNameTextView.setText(userLastName);
+                }else {
+                    Log.d(TAG, "Last name null");
+                    lastNameTextView.setText(lastNameString);
+                }
+                if(!TextUtils.isEmpty(userMobileNumber)){
+                    mobileNoTextView.setText(userMobileNumber);
+                }else {
+                    Log.d(TAG, "Mobile number null");
+                    mobileNoTextView.setText(mobileNumberString);
+                }
             }else {
-                Log.d(TAG, "First name null");
+                Log.d(TAG, "UserDetails Null...");
                 firstNameTextView.setText(firstNameString);
-            }
-            if(!TextUtils.isEmpty(userDetails.getLastName())){
-                Log.d(TAG, "Last name: "+userDetails.getLastName());
-                lastNameTextView.setText(userDetails.getLastName());
-            }else {
-                Log.d(TAG, "Last name null");
                 lastNameTextView.setText(lastNameString);
-            }
-            if(!TextUtils.isEmpty(userDetails.getMobileNumber())){
-                Log.d(TAG, "Mobile number: "+userDetails.getMobileNumber());
-                mobileNoTextView.setText(userDetails.getMobileNumber());
-            }else {
-                Log.d(TAG, "Mobile number null");
                 mobileNoTextView.setText(mobileNumberString);
             }
 
